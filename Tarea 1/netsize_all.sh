24038737e092b4475cf58a007c07caf15217c6c1
Antonella Ortiz <maria.ortiz@est.ikiam.edu.ec>
2021-12-10 10:01:57 -0500

    Arreglo del loop

--- Tarea 1/netsize_all.sh
+++ Tarea 1/netsize_all.sh
@@ -4,4 +4,4 @@
 archivo=*.txt
 echo $archivo
 
-for data in $archivo; do f=(wc -l $data); c=(head -n 1 $data | tr -d " " | tr -d "\n" | wc -c); echo $data; done
+for data in $archivo; do f=$(wc -l $data); c=$(head -n 1 $data | tr -d " " | tr -d "\n" | wc -c); echo $data $f $c; done
