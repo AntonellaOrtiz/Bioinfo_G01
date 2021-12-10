f1685b47ffee44480d571d71415efce29bdb6054
Antonella Ortiz <maria.ortiz@est.ikiam.edu.ec>
2021-12-09 21:21:09 -0500

    Cambio de variable Nombre por n

--- Tarea 1/netsize.sh
+++ Tarea 1/netsize.sh
@@ -6,8 +6,8 @@ wc -l n27.txt
 head -n 1 n27.txt | tr -d " " | tr -d "\n" | wc -c
 
 # falta imprimir contenido
-Nombre=n27.txt
+n=n27.txt
 f=$(wc -l n27.txt)
 c=$(head -n 1 n27.txt | tr -d " " | tr -d "\n" | wc -c)
 
-echo "Nombre=$Nombre" "Filas=$f" "Columnas=$c"
+#echo "Nombre=$Nombre" "Filas=$f" "Columnas=$c"
