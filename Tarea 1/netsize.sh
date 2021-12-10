1d4ff351849d38cdc17cd929cebdc402122d4e75
Antonella Ortiz <maria.ortiz@est.ikiam.edu.ec>
2021-12-09 21:52:44 -0500

    Modificación del documento

--- Tarea 1/netsize.sh
+++ Tarea 1/netsize.sh
@@ -10,5 +10,7 @@ n=n27.txt
 f=$(wc -l n27.txt)
 c=$(head -n 1 n27.txt | tr -d " " | tr -d "\n" | wc -c)
 
-echo "Nombre=$n" "Filas=$f" "Columnas=$c" > resultados.txt
+echo "Nombre=$n" > resultados.txt
+echo "Filas=$f" >> resultados.txt
+echo "Columnas=$c" >> resultados.txt
 cat resultados.txt
