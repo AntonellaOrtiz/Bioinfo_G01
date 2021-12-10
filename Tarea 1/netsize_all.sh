f43427ca0ff02ebab3ceb3da1144e2c9d25f24db
Antonella Ortiz <maria.ortiz@est.ikiam.edu.ec>
2021-12-10 15:34:49 -0500

    Cambio de nombre del archivo para resultados x2

--- Tarea 1/netsize_all.sh
+++ Tarea 1/netsize_all.sh
@@ -7,7 +7,7 @@ echo $archivo
 for data in $archivo; do f=$(wc -l $data); c=$(head -n 1 $data | tr -d " " | tr -d "\n" | wc -c); echo $data $f $c; done
 
 #para guardar en un archivo
-for data in $archivo; do f=$(wc -l $data); c=$(head -n 1 $data | tr -d " " | tr -d "\n" | wc -c); echo $data $f $c >> ../../../../resultados2.txt; done
+for data in $archivo; do f=$(wc -l $data); c=$(head -n 1 $data | tr -d " " | tr -d "\n" | wc -c); echo $data $f $c >> ../../../../netsize_all.txt; done
 
 #Mostrar el contenido del archivo
-cat ../../../../resultados2.txt
+cat ../../../../netsize_all.txt
