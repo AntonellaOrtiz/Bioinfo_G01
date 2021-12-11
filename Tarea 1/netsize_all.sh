fe482ff11a53022b9c0e151d6ac902b26fa9aada
Antonella Ortiz <maria.ortiz@est.ikiam.edu.ec>
2021-12-11 11:34:02 -0500

    Agregar los comentarios necesarios

--- Tarea 1/netsize_all.sh
+++ Tarea 1/netsize_all.sh
@@ -1,12 +1,19 @@
 #Ejercicio 1.10.3
 #Parte 2
 
+#Crear variable archivo con todos los *.txt de la carpeta Saavedra2013
 archivo=*.txt
+#echo: imprime lo contenido en la variable
 echo $archivo
 
+#Crear el loop con el código hecho en netsize.sh, donde:
+#for: data en la variable $archivo creada anteriormente
+#do: ejecución de los comandos necesarios y echo para imprimirlos
+#done: termina el loop
 for data in $archivo; do f=$(wc -l $data); c=$(head -n 1 $data | tr -d " " | tr -d "\n" | wc -c); echo $data $f $c; done
 
 #para guardar en un archivo
+#agregar >> para guardar la info en el archivo netsize_all.txt
 for data in $archivo; do f=$(wc -l $data); c=$(head -n 1 $data | tr -d " " | tr -d "\n" | wc -c); echo $data $f $c >> ../../../../netsize_all.txt; done
 
 #Mostrar el contenido del archivo
